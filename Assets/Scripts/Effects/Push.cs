dfa535c406e25acffec9ea86d04f7b02dc6b5bf7
manzano alban <manzanoalban@gmail.com>
2017-12-07 19:12:15 +0100

    voir au dessus

---
+++ Assets/Scripts/Effects/Push.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : Effect
+{
+    private float damage;
+    private int nbCases;
+    private Game gameManager;
+    /// <summary>
+    /// Un super push détruit le bloc et augmente les dégats
+    /// </summary>
+    private bool isSuper;
+    /// <summary>
+    /// Crée un objet de dégats
+    /// </summary>
+    /// <param name="cible">Indique la cible du coup</param>
+    /// <param name="lanceur">Indique le lanceur du coup</param>
+    public Push(Placeable cible, Placeable lanceur, int nbCases, bool isSuper) : base(cible, lanceur)
+    {
+        this.nbCases = nbCases;
+        this.isSuper = isSuper;
+    }
+
+
+
+
+    override
+        public void Use()
+    {
+        if (Cible != Lanceur)
+        {
+            Vector3Int vectDiff = Cible.Position - Lanceur.Position;
+            if (vectDiff.magnitude == 1)
+            {
+
+                for (int i = 0; i < nbCases; i++)
+                {
+                    Vector3Int posConsidered = Cible.Position + vectDiff;
+                    if (gameManager.GrilleJeu.Grid[posConsidered.x, posConsidered.y, posConsidered.z] == null)
+                    {
+                        gameManager.GrilleJeu.DeplaceBloc(Cible, posConsidered);
+
+                    }
+                    else if (gameManager.GrilleJeu.Grid[posConsidered.x, posConsidered.y, posConsidered.z].GetType() == typeof(LivingPlaceable))
+                    {
+                        if (isSuper)
+                        {
+                            if (Cible.GetType() == typeof(LivingPlaceable))
+                            {
+                                gameManager.GameEffectManager.ToBeTreated.Add(new Damage(Cible, Lanceur, nbCases * 35));
+                            }
+                            gameManager.GameEffectManager.ToBeTreated.Add(new Damage(gameManager.GrilleJeu.Grid[posConsidered.x, posConsidered.y, posConsidered.z], Cible, nbCases * 35));
+                            break;
+                        }
+                        else
+                        {
+                            if (Cible.GetType() == typeof(LivingPlaceable))
+                            {
+                                gameManager.GameEffectManager.ToBeTreated.Add(new Damage(Cible, Lanceur, nbCases * 20));
+                            }
+                            gameManager.GameEffectManager.ToBeTreated.Add(new Damage(gameManager.GrilleJeu.Grid[posConsidered.x, posConsidered.y, posConsidered.z], Cible, nbCases * 20));
+                            break;
+                        }
+                    }
+                    else if (gameManager.GrilleJeu.Grid[posConsidered.x, posConsidered.y, posConsidered.z].GetType() == typeof(Placeable))
+                    {
+                        
+                            if (isSuper)
+                            {
+                            //on deplace le placeable
+                            gameManager.GrilleJeu.DeplaceBloc(Cible, posConsidered);
+                                if (Cible.GetType() == typeof(LivingPlaceable))
+                                {
+                                    gameManager.GameEffectManager.ToBeTreated.Add(new Damage(Cible, Lanceur, nbCases * 35));
+                                }
+                            }
+                            else //on applique des dommages réduit si il y a lieu
+                            {
+                                if (Cible.GetType() == typeof(LivingPlaceable))
+                                {
+                                    gameManager.GameEffectManager.ToBeTreated.Add(new Damage(Cible, Lanceur, nbCases * 20));
+                                }
+
+                                break;
+                            }
+                            }
+                          
+                        }
+
+                    
+
+                
+            }
+        }
+    }
+}
