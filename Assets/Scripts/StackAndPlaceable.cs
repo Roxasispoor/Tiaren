75d3cd6fbb2196eb97d5748ce65a001e2c8c704e
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-10-19 00:23:36 +0200

    Tour d'observation

---
+++ Assets/Scripts/StackAndPlaceable.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackAndPlaceable {
+
+    private LivingPlaceable character;
+    private float speedStack;
+    private bool seenBefore;
+
+
+
+    public StackAndPlaceable(LivingPlaceable character, float speedStack, bool seenBefore)
+    {
+        this.Character = character;
+        this.SpeedStack = speedStack;
+        this.SeenBefore = seenBefore;
+    }
+
+    public float SpeedStack
+    {
+        get
+        {
+            return speedStack;
+        }
+
+        set
+        {
+            speedStack = value;
+        }
+    }
+
+    public LivingPlaceable Character
+    {
+        get
+        {
+            return character;
+        }
+
+        set
+        {
+            character = value;
+        }
+    }
+
+    public bool SeenBefore
+    {
+        get
+        {
+            return seenBefore;
+        }
+
+        set
+        {
+            seenBefore = value;
+        }
+    }
+}
