658422e3e6be1b792dd4c99c3662f92ef4f2916e
manzano alban <manzanoalban@gmail.com>
2018-11-06 14:30:22 +0100

    batching init au minimum et prévu pour la suite

---
+++ Assets/Scripts/Effects/EffectManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager:MonoBehaviour {
+    public static EffectManager instance;
+    void Awake()
+    {
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+    }
+    public void UseEffect(Effect effect)
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
