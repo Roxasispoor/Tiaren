b8502aad472238fc725202889dc8c96345e95df3
manzano alban <manzanoalban@gmail.com>
2019-01-15 18:50:44 +0100

    reconnection is up there in our hearts and somewhere in the code

---
+++ Assets/Scripts/reseauScripts/TCPTransmitter.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TCPTransmitter : MonoBehaviour
+{
+    #region private members
+    public TcpClient socketConnection;
+    private Thread clientReceiveThread;
+    public NetworkManager networkManager;
+    public String clientMessage;
+    /// <summary> 	
+    /// TCPListener to listen for incomming TCP connection 	
+    /// requests. 	
+    /// </summary> 	
+    private TcpListener tcpListener;
+    /// <summary> 
+    /// Background thread for TcpServer workload. 	
+    /// </summary> 	
+    private Thread tcpListenerThread;
+    /// <summary> 	
+    /// Create handle to connected tcp client. 	
+    /// </summary> 	
+    private TcpClient connectedTcpClient;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        // Start TcpServer background thread 		
+    }
+    public void initServer()
+    {
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendMessage();
+        }
+    }
+
+    /// <summary> 	
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
+    /// </summary> 	
+    private void ListenForIncommingRequests()
+    {
+        try
+        {
+            string adress = networkManager.networkAddress;
+            if(adress=="localhost")
+            {
+                adress = "127.0.0.1";
+            }
+            // Create listener on localhost port 8052.
+            Debug.LogError(networkManager.networkAddress);
+
+            tcpListener = new TcpListener(IPAddress.Parse(adress), 8052);
+            tcpListener.Start();
+            Debug.LogError("Server is listening");
+            Byte[] bytes = new Byte[1024];
+         
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    
+                    // Get a stream object for reading 					
+                    using (NetworkStream stream = connectedTcpClient.GetStream())
+                    {
+                       
+                        int length;
+                        // Read incomming stream into byte arrary. 						
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            // Convert byte array to string message. 							
+                            clientMessage = Encoding.ASCII.GetString(incommingData);
+                             Debug.LogError("client message received as: " + clientMessage);
+                           
+
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+    }
+    /// <summary> 	
+    /// Send message to client using socket connection. 	
+    /// </summary> 	
+    private void SendMessage()
+    {
+        if (connectedTcpClient == null)
+        {
+            return;
+        }
+
+        try
+        {
+            // Get a stream object for writing. 			
+            NetworkStream stream = connectedTcpClient.GetStream();
+            if (stream.CanWrite)
+            {
+                string serverMessage = "This is a message from your server.";
+                // Convert string message to byte array.                 
+                byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                // Write byte array to socketConnection stream.               
+                stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+                Debug.LogError("Server sent his message - should be received by client");
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+    public void ConnectToTcpServer()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+    }
+    /// <summary> 	
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+    /// </summary>     
+    private void ListenForData()
+    {
+        try
+        {
+            socketConnection = new TcpClient("localhost", 8052);
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                // Get a stream object for reading 				
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 						
+                        string serverMessage = Encoding.ASCII.GetString(incommingData);
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+    /// <summary> 	
+    /// Send message to server using socket connection. 	
+    /// </summary> 	
+    public void SendMessageToServer()
+    {
+        if (socketConnection == null)
+        {
+            Debug.Log("Socket was null");
+            return;
+        }
+        try
+        {
+            // Get a stream object for writing. 			
+            NetworkStream stream = socketConnection.GetStream();
+            if (stream.CanWrite)
+            {
+                string clientMessage = "This is a message from one of your clients.";
+                // Convert string message to byte array.                 
+                byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+                // Write byte array to socketConnection stream.                 
+                stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+                Debug.Log("Client sent his message - should be received by server");
+            }
+            else
+            {
+                Debug.Log("Couldn't write");
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+}
---
+++ Assets/Scripts/reseauScripts/TransmitterNoThread.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TransmitterNoThread : MonoBehaviour {
+
+    public TcpClient client;
+    public TcpListener tcpListener;
+    public TcpClient server;
+    public NetworkManager networkManager;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    //TcpClient client
+    public IEnumerator AcceptTcp()
+    {
+
+        string adress = networkManager.networkAddress;
+        if (adress == "localhost")
+        {
+            adress = "127.0.0.1";
+        }
+        tcpListener = new TcpListener(IPAddress.Parse(adress), networkManager.matchPort);
+        tcpListener.Start();
+        while (client == null)
+        {
+            //Console.Write("Waiting for a connection... ");
+
+            // Perform a blocking call to accept requests.
+            // You could also user server.AcceptSocket() here.
+            if (tcpListener.Pending())
+            {
+                Debug.LogError("Client detected");
+                client = tcpListener.AcceptTcpClient();
+                SendData();
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+    public void SendData()
+    {
+        if (client == null)
+        {
+            Debug.LogError("Client was null when sending data");
+        }
+        else
+        { 
+
+            try
+            {
+                // Get a stream object for writing. 			
+                NetworkStream stream = client.GetStream();
+                if (stream.CanWrite)
+                {
+                    string serverMessage = "This is a message from your server.";
+                    // Convert string message to byte array.                 
+                    byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                    // Write byte array to socketConnection stream.               
+                    stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+                    Debug.LogError("Server sent his message - should be received by client");
+                    stream.Close();
+                }
+            }
+            catch (SocketException socketException)
+            {
+                Debug.Log("Socket exception: " + socketException);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Connect to server and listen for the data incoming until it receive EOF
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator ListenToData()
+    {
+              server = new TcpClient("localhost", networkManager.matchPort);
+            Byte[] bytes = new Byte[1024];
+            string serverMessage="";
+        using (NetworkStream stream = server.GetStream())
+        {
+            while (!serverMessage.EndsWith("///"))
+            {
+            try
+            {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while (stream.DataAvailable && (length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 						
+                        serverMessage = Encoding.ASCII.GetString(incommingData);
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                
+            }
+            catch (SocketException socketException)
+            {
+                Debug.Log("Socket exception: " + socketException);
+            }
+            yield return null;
+        }
+        }
+        Debug.Log("Coroutine ended");
+
+    }
+
+}
