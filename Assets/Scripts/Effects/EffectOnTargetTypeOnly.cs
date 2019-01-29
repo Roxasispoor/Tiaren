3b96094baf7a2f89ba9e38e308f27e3a95ea9343
Moux <maxthiry@hotmail.fr>
2019-01-29 19:48:34 +0100

    Tourbilol et avancée zones d'effets

---
+++ Assets/Scripts/Effects/EffectOnTargetTypeOnly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that is used for effect that must work on mother class but not child class
+/// </summary>
+/// <typeparam name="L"></typeparam>
+/// <typeparam name="T"></typeparam>
+public abstract class EffectOnTargetTypeOnly<L,T> : EffectV2<L,T> where L:NetIdeable where T:NetIdeable
+{
+    public override void TargetAndInvokeEffectManager(T target)
+    {
+        if(target.GetType().IsSubclassOf(typeof(T)))
+        {
+
+        }
+        else
+        { 
+        Target = target;
+        }
+    }
+}
