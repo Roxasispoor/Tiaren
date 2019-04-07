a48384df01a1fdce3679429b6ff0c95f2ffa2620
manzano alban <manzanoalban@gmail.com>
2019-04-07 10:30:53 +0200

    normalize spawning place of cubes

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -191,7 +191,7 @@ public class Grid : MonoBehaviour
         }
         if (CheckNull(position))
         {
-            GameObject newBlock = Instantiate(prefab, new Vector3(position.x, position.y, position.z), Quaternion.identity, Grid.instance.transform);
+            GameObject newBlock = Instantiate(prefab, new Vector3(position.x, position.y, position.z), Quaternion.identity, GameManager.instance.gridFolder.transform);
             gridMatrix[position.x, position.y, position.z] = newBlock.GetComponent<Placeable>();
 
             Placeable blockUnder = GetPlaceableFromVector(position + Vector3Int.down);
