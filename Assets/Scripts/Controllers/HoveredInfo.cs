ba9742194e16ba14c3b5cc143c15649b529d7fe2
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2019-04-15 13:58:50 +0200

    Created the HoveredInfo struct

---
+++ Assets/Scripts/Controllers/HoveredInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Struct containing the info of the placeable hitted by the RecastSelector
+/// </summary>
+public struct HoveredInfo
+{
+    public HoveredInfo (Placeable placeable, Vector3 face)
+    {
+        this.placeable = placeable;
+        this.face = face;
+    }
+
+    /// <summary>
+    /// The placeable hitted.
+    /// </summary>
+    Placeable placeable;
+    /// <summary>
+    /// The face hitted.
+    /// </summary>
+    Vector3 face;
+}
